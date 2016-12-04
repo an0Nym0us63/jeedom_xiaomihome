207c42b9e3b6a4175ce45239831ee34ab36984ad
Cédric Dupont <lunarok@gmail.com>
2016-12-04 19:33:21 +0100

    python

--- resources/xiaomihome.py
+++ resources/xiaomihome.py
@@ -7,7 +7,8 @@ import requests
 def push_data(data):
 	r = requests.post(sys.argv[1], json=data, timeout=(0.5, 120), verify=False)
 
-connector = XiaomiConnector(data_callback=push_data)
+if __name__ == '__main__':
+	connector = XiaomiConnector(data_callback=push_data)
 
-while True:
-	connector.check_incoming()
+	while True:
+		connector.check_incoming()
