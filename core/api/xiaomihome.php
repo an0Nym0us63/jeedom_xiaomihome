eb1a6b3c2c85c53a29d5ad10261479da5fe72898
Cédric Dupont <lunarok@gmail.com>
2016-12-17 08:50:09 +0100

    correct heartbeat

--- core/api/xiaomihome.php
+++ core/api/xiaomihome.php
@@ -1,43 +1,47 @@
 <?php
 
 /* This file is part of Jeedom.
- *
- * Jeedom is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Jeedom is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
- */
- require_once dirname(__FILE__) . "/../../../../core/php/core.inc.php";
+*
+* Jeedom is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Jeedom is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+*/
+require_once dirname(__FILE__) . "/../../../../core/php/core.inc.php";
 
- if (!jeedom::apiAccess(init('apikey'), 'xiaomihome')) {
- 	echo __('Clef API non valide, vous n\'êtes pas autorisé à effectuer cette action (xiaomihome)', __FILE__);
- 	die();
- }
+if (!jeedom::apiAccess(init('apikey'), 'xiaomihome')) {
+    echo __('Clef API non valide, vous n\'êtes pas autorisé à effectuer cette action (xiaomihome)', __FILE__);
+    die();
+}
 
- $body = json_decode(file_get_contents('php://input'), true);
- if (init('sid') !== null && init('model') !== null) {
-     if (init('cmd') == 'heartbeat') {
-         xiaomihome::receiveHeartbeat(init('sid'), init('model'), $body['ip']);
-     } else {
-         xiaomihome::receiveId(init('sid'), init('model'));
-          log::add('xiaomihome', 'debug', 'Recu ' . init('sid') . ' ' . init('model') . ' ' . print_r($body, true));
-         if (is_array($body)) {
-             foreach ($body as $key => $value) {
-                 xiaomihome::receiveData(init('sid'), init('model'), $key, $value);
-             }
-         }
-     }
- }
+$body = json_decode(file_get_contents('php://input'), true);
+if (init('sid') !== null && init('model') !== null) {
+    if (init('cmd') == 'heartbeat') {
+        if (init('cmd') == 'heartbeat') {
+            xiaomihome::receiveHeartbeat(init('sid'), init('model'), $body['ip']);
+        } else {
+            xiaomihome::receiveId(init('sid'), init('model'));
+        }
+    } else {
+        xiaomihome::receiveId(init('sid'), init('model'));
+        log::add('xiaomihome', 'debug', 'Recu ' . init('sid') . ' ' . init('model') . ' ' . print_r($body, true));
+        if (is_array($body)) {
+            foreach ($body as $key => $value) {
+                xiaomihome::receiveData(init('sid'), init('model'), $key, $value);
+            }
+        }
+    }
+}
 
 
- return true;
+return true;
 
 ?>
